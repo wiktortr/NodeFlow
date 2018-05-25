f09dcb0d4270b8c6323d7f81a63056f8660fd50e
wiktortr <wiktortr9@gmail.com>
2018-05-25 20:14:25 +0200

    Go To Electron

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const url = require("url");
+const electron = require("electron");
+const app = electron.app;
+
+app.on("ready", function(){
+
+    let mainwindow = new electron.BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    mainwindow.loadURL(url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+
+    mainwindow.on("closed", function(){
+        mainwindow = null;
+        app.exit();
+    }); 
+
+});
